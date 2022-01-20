2f44a91812cd0f0603b898539e86b12032b7c3ad
jason-tung <jt568@cornell.edu>
2022-01-20 10:49:55 -0500

    pages

--- scripts/headers.js
+++ scripts/headers.js
@@ -1,7 +1,7 @@
 const enableHeader = (cat) => {
-    $("#header").load("./templates/header.html", () => {
+    $("#header").load("/templates/header.html", () => {
         $(cat).removeClass("inactive-link");
         $(cat).addClass("active-link");
     });
-    $("#footer").load("./templates/footer.html");
+    $("#footer").load("/templates/footer.html");
 }
